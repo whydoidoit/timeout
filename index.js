54e78176de01e26ff8b35e015f3b03eb4c574e18
Mike Talbot <mike.talbot@3radical.com>
2017-09-10 12:32:59 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,30 @@
+import update from 'update'
+
+let app = pc.Application.getApplication()
+
+function timeout(fn, delay) {
+    delay = delay || 0;
+    var t = 0;
+    var exit = false;
+    function reset() {
+        exit = true
+    }
+    app.on('clear', reset)
+    update(function (dt) {
+        if (exit) {
+            app.off('clear', reset)
+            return false;
+        }
+        t += dt * 1000;
+        if (t >= delay) {
+            fn();
+            app.off('clear', reset)
+            return false;
+        }
+    });
+    return reset
+}
+
+pc.timeout = timeout
+
+export default timeout
